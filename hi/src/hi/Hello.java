003d3b6a9f856242cbdf0f2be7e1d0382f243ca9
enohs <kmsn01@naver.com>
2024-09-05 01:09:03 +0900

    My Second Commit!

--- hi/src/hi/Hello.java
+++ hi/src/hi/Hello.java
@@ -2,8 +2,9 @@ package hi;
 
 public class Hello {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.println("Well I need to commit again");
+  }
 
 }
