6f0b92daeacbc2ac762d736cb23c4ce8434ca1de
enohs <kmsn01@naver.com>
2024-09-05 01:11:10 +0900

    gogo

--- hi/src/hi/Hello.java
+++ hi/src/hi/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello World");
     System.out.println("Well I need to commit again");
+    System.out.println("Why it doesn't work?");
   }
 
 }
